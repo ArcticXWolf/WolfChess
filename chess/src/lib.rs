c980b9896ddc6ce49b4204e14b8d38fa3eacbff5
Jan Niklas Richter <git@janniklasrichter.de>
2022-05-26 10:35:06 +0200

    Initial commit

---
+++ chess/src/lib.rs
@@ -0,0 +1,8 @@
+#[derive(Debug)]
+pub struct Board {}
+
+impl Board {
+    pub fn render(&self) -> () {
+        println!("Board would render here.");
+    }
+}
---
+++ cui/src/main.rs
@@ -0,0 +1,7 @@
+use chess;
+
+fn main() {
+    let board = chess::Board {};
+    println!("Hello, world!");
+    board.render();
+}
